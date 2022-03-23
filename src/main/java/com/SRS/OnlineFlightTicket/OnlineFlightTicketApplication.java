ec5b7b2d6a93906fb29deaa8c92fcc7d2834fb5e
unknown <oggusaikumar@gmail.com>
2022-03-23 19:31:38 +0530

    first commit

---
+++ src/main/java/com/SRS/OnlineFlightTicket/OnlineFlightTicketApplication.java
@@ -0,0 +1,13 @@
+package com.SRS.OnlineFlightTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFlightTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFlightTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SRS/OnlineFlightTicket/OnlineFlightTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SRS.OnlineFlightTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFlightTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
